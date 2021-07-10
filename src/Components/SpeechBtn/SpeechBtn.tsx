0e01a745f9a518f5e7f2d44d86e8d86ff94b00ae
Hound404 <wildhound404@gmail.com>
2021-07-10 19:30:18 +0600

    sentence & word highlight added

--- src/Components/SpeechBtn/SpeechBtn.tsx
+++ src/Components/SpeechBtn/SpeechBtn.tsx
@@ -1,18 +1,70 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { GlobalState } from "../../App";
 import styles from "./SpeechBtn.module.scss";
 
 const SpeechBtn = () => {
   const { inputVal, selectedVoice, rate, pitch } = useContext(GlobalState);
 
+  let currentSnetence = -1;
+  let textHighlight = "";
+
   const speechUtterence = new SpeechSynthesisUtterance();
+
+  speechUtterence.addEventListener("boundary", (e) => {
+    let textArray = inputVal.split("");
+    let currentWord = textArray.splice(e.charIndex, e.charLength).join("");
+
+    if (currentWord === "") {
+      currentSnetence = ++currentSnetence;
+      highlightCurrentSentence();
+    } else {
+      highlightCurrentWord(e.charIndex, e.charLength, currentWord);
+      console.log(currentWord);
+    }
+  });
+  speechUtterence.addEventListener("end", (e) => {
+    currentSnetence = -1;
+  });
+
+  const highlightCurrentSentence = () => {
+    const textArray = inputVal.split(".");
+    let initPart = textArray.slice(0, currentSnetence);
+    let middlePart: string[] = [];
+    if (currentSnetence === 0) {
+      middlePart.push(
+        `<span class="sentenceHighlight">${textArray[currentSnetence]}</span>`
+      );
+    } else {
+      middlePart.push(
+        `<span class="sentenceHighlight">${textArray[currentSnetence]}</span>`
+      );
+    }
+
+    let lastPart = textArray.splice(currentSnetence + 1, textArray.length - 1);
+    let combined = [...initPart, ...middlePart, ...lastPart];
+    textHighlight = combined.join(".");
+    TextOporation(textHighlight);
+  };
+
+  const highlightCurrentWord = (
+    index: number,
+    length: number,
+    currentWord: string
+  ) => {
+    const textArray = textHighlight.split("");
+    let initPart = textArray.slice(0, index + 32);
+    let middlePart = [`<span class="wordHighlight">${currentWord}</span>`];
+    let lastPart = textArray.slice(index + 32 + length, textArray.length - 1);
+    let combined = [...initPart, ...middlePart, ...lastPart];
+    // debugger;
+    TextOporation(combined.join(""));
+  };
 
   const speakAtc = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     if (inputVal === "") {
       return;
     }
     Speak();
-    TextOporation();
   };
 
   const Speak = () => {
@@ -26,12 +78,11 @@ const SpeechBtn = () => {
       }
     });
     speechSynthesis.speak(speechUtterence);
-    console.log(selectedVoice);
   };
 
-  const TextOporation = () => {
+  const TextOporation = (insertHTML: string) => {
     const element = document.getElementById("resultsArea");
-    element!.innerHTML = inputVal;
+    element!.innerHTML = insertHTML;
   };
 
   return (
